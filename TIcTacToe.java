83d1401507d25f3d53812ca6ec60c6aa89fb9bd1
Ayush Singh <ayush.singh@ET-C02Z1C3HLVCF.local>
2020-01-25 13:23:17 +0530

    this is the main class that runs and intialize the process

---
+++ TIcTacToe.java
@@ -0,0 +1,19 @@
+
+
+public class TIcTacToe {
+
+
+    public static void main(String args[]){
+
+
+
+       game g = new game(3);
+
+       Human h1= new Human();
+       Human h2 = new Human();
+       g.humanTouman(h1,h2);
+
+
+    }
+
+}
