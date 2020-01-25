1143e9e774f5b339bfbb2fbdb450337da9369985
Ayush Singh <ayush.singh@ET-C02Z1C3HLVCF.local>
2020-01-25 21:48:04 +0530

    attaching after some modification on human class

--- Human.java
+++ Human.java
@@ -3,22 +3,31 @@ import java.util.Scanner;
 public class Human extends Player{
 
 
+
     @Override
-     public void move(board newboard) {
+     public void move(board newboard,int value) {
 
              Scanner sc = new Scanner(System.in);
              System.out.println("enter your move in 3 X 3 matrix with i and j values of matrix");
              System.out.println("follwed by 1 for player 1 and 2 for player 2");
              int i = sc.nextInt();
              int j = sc.nextInt();
-             int value = sc.nextInt();
-             newboard.place(i,j,value);
+
+
             //need to check the validation of i j and value
-      //   sc.close();
-            if(newboard.isValid(newboard.gameboard,i,j,value))
-         newboard.printboard(newboard.gameboard);
-             else
-                System.out.println("unexpected output");
+                 // sc.close();
+
+            if(newboard.isValid(newboard,i,j,value) ) {
+                newboard.place(i,j,value);
+                newboard.printboard(newboard.gameboard);
+
+            }
+             else {
+                System.out.println("unexpected input");
+              this.move(newboard,value);
+            }
+
+
 
      }
 
