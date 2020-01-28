c989a645dc78275720b47f1e0c3a188348d3963c
Ayush Singh <ayush.singh@ET-C02Z1C3HLVCF.local>
2020-01-28 17:44:38 +0530

    #    Feature with 3X3 (MOdified)

---
+++ GenralGame/MainClass.java
@@ -0,0 +1,30 @@
+package GenralGame;
+    import Game9X9.*;
+
+    import Game3X3.* ;
+
+    import java.util.Scanner;
+
+public class MainClass {
+
+
+
+public static void main(String args[])
+
+{
+
+    Scanner sc =new Scanner(System.in);
+    System.out.println("enter the value of n on nXn grid");
+    int n = sc.nextInt();
+    if(n%3!=0)
+        System.out.println("please enter the muptiple of three");
+    else
+    {
+        game_nXn g = new game_nXn(n);
+        g.run();
+    }
+
+}
+
+
+}
